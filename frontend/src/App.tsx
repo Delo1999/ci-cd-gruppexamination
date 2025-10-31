f9db3d236b2996b01fcf7ed339ad979765decc74
Delo <dilovanhassan@hotmail.com>
2025-10-31 16:47:00 +0100

    4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <h1>Meetup App</h1>
-        <p>Frontend deployment till AWS S3 är konfigurerad!</p>
+        <p>Frontend deployment till AWS S3 är konfigurerad!!!!!</p>
         <p>API URL: {process.env.REACT_APP_API_URL || "Inte satt"}</p>
       </header>
     </div>
